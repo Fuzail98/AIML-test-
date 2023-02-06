8798eaf06b908ea6fd1e42d46d8aed5390aee42b
Fuzail98 <fuzi98@gmail.com>
2023-02-06 13:21:54 +0530

    Async run_fd_time

--- test/main.py
+++ test/main.py
@@ -7,17 +7,21 @@ from faceDetection import runFaceDetection
 import asyncio
 
 
+async def run_fd_time(frame):
+    task1 = asyncio.create_task(addTimeStamp(frame)) 
+    task2 = asyncio.create_task(runFaceDetection(frame))
+    await asyncio.gather(task1, task2)
+    await asyncio.sleep(0.01)
+
+
 async def main(captured_obj):
     while True:
         async for camera_name, cap in captured.asyncCameraGen():
             frame = await captured.read_frame(cap)
             # frame = await addTimeStamp(frame)
             # frame = runFaceDetection(frame)
-            
-            task1 = asyncio.create_task(addTimeStamp(frame)) 
-            task2 = asyncio.create_task(runFaceDetection(frame))
-            await asyncio.gather(task1, task2)
 
+            await run_fd_time(frame)
             await captured_obj.showFrame(camera_name, frame)
             # cv.imshow(camera_name, frame)
 
